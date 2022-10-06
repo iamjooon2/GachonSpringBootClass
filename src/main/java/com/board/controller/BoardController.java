dc634c2ed6e9d6773f43c07972f3fb882c614526
iamjooon2 <iamjooon2@gmail.com>
2022-10-06 14:36:18 +0900

    week5: 클라이언트 뷰인 list.html, view.html 추가 및 Controller 코드 추가, MySQL Connection 위한 jdbc 옵션 추가

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -11,16 +11,42 @@ import com.board.domain.BoardDTO;
 
 import com.board.service.BoardService;
 
+import java.util.List;
+
 
 @Controller
 public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    @GetMapping(value="/board/view.do")
+    public String openBoardDetail(@RequestParam(value="idx", required = false) Long idx, Model model){
+        if (idx == null) {
+            // TODO: 올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+            return "redirect:/board/list.do";
+        }
+        BoardDTO board = boardService.getBoardDetail(idx);
+        if (board == null || "Y".equals(board.getDeleteYn())){
+            // TODO: 없는 게시글이거나, 이미 삭제된 게시글이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+            return "redirect:/board/list.do";
+        }
+        model.addAttribute("board", board);
+
+        return "board/view";
+    }
+
+    @GetMapping(value = "/board/list.do")
+    public String openBoardList(Model model) {
+        List<BoardDTO> boardList = boardService.getBoardList();
+        model.addAttribute("boardList", boardList);
+
+        return "board/list";
+    }
+
     @GetMapping(value = "/board/write.do")
     public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
-        if(idx==null) {
-            model.addAttribute("board", new BoardDTO());
+        if (idx==null) {
+            model.addAttribute("board", new BoardDTO() );
         } else {
             BoardDTO board = boardService.getBoardDetail(idx);
             if(board==null) {
@@ -32,6 +58,7 @@ public class BoardController {
         return "board/write";
     }
 
+    // 게시글 수정시
     @PostMapping(value="/board/register.do")
     public String registerBoard(final BoardDTO params) {
         try {
