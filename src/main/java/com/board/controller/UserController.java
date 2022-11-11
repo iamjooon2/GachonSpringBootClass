a1bc18066cbc096a70a0a18f1375702149e6d34f
iamjooon2 <iamjooon2@gmail.com>
2022-11-11 20:30:19 +0900

    feat: 로그인시 존재하지 않는 사용자일때 로그인창 리턴 기능 구현

--- src/main/java/com/board/controller/UserController.java
+++ src/main/java/com/board/controller/UserController.java
@@ -1,20 +1,55 @@
 package com.board.controller;
 
-import com.board.domain.BoardDTO;
+import com.board.constant.Method;
+import com.board.domain.UserDTO;
 import com.board.service.UserService;
+import com.board.util.UiUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-public class UserController {
+@Controller
+public class UserController extends UiUtils {
+
     @Autowired
     private UserService userService;
 
-    @GetMapping(value = "/")
-    public String redirectToLoginPage(@ModelAttribute("params") BoardDTO params, Model model) {
-        model.addAttribute("loginPage");
-
-        return "bamboo/login";
+    @GetMapping(value = "/signUp")
+    public String signInPage() {
+        return "signUp";
     }
+
+    @GetMapping(value = "/index")
+    public String loginPage() {
+        return "index";
+    }
+
+    @GetMapping(value = "/main")
+    public String mainPage() {
+        return "board/list";
+    }
+
+//    @PostMapping(value = "/user/register")
+//    public String registerUser(UserDTO param){
+//        UserDTO param = new UserDTO();
+//
+//        return "redirect:/";
+//    }
+//
+
+    @PostMapping(value = "/user/login")
+    public String loginUser(@ModelAttribute("params") UserDTO params, @RequestParam(value = "idx", required = false) Long idx, Model model) {
+        if (userService.checkUserNameExists(params)
+                && userService.checkUserNameExists(params)) {
+            return "board/list";
+        }
+        return showMessageWithRedirect("존재하지 않는 사용자입니다", "/", Method.GET, null, model);
+    }
+
+
+
 }
