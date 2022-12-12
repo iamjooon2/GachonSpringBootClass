bd1598ab7aaaea3d9035d38f5074c20bb8c7b86c
iamjooon2 <iamjooon2@gmail.com>
2022-12-12 15:21:14 +0900

    chore: 어플리케이션 실행시 메모리 사용량 보여주기

--- src/main/java/com/board/BoardApplication.java
+++ src/main/java/com/board/BoardApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class, args);
+
+        // 메모리 사용량 출력하기
+        long heapSize = Runtime.getRuntime().totalMemory();
+        System.out.println("Heap Size(M): " + heapSize / (1024 * 1024));
+    }
 
 }
